79a715ff3dfe94ee4dc898d9d4d2bf4aa338f33f
synw <synwx@protonmail.com>
2023-09-10 16:27:46 +0200

    Add api doc

--- src/cls.ts
+++ src/cls.ts
@@ -1,6 +1,12 @@
 import { LmTemplate, PromptBlock, TurnBlock, SpacingSlots } from "./interfaces";
 import { templates } from "./db.js";
 
+/**
+ * Represents a modified language model template.
+ * 
+ * @example
+ * const tpl = new ModTemplate('alpaca');
+ */
 class ModTemplate implements LmTemplate {
   name: string;
   user: string;
@@ -13,6 +19,14 @@ class ModTemplate implements LmTemplate {
   // internal state
   private _systemBlock = "";
 
+  /**
+   * Constructs a new `ModTemplate` instance.
+   * 
+   * @param template - Either the name of the template to load or an instance of `LmTemplate`.
+   * 
+   * @example
+   * const tpl = new ModTemplate('alpaca');
+   */
   constructor(template: string | LmTemplate) {
     let tpl: LmTemplate
     if (typeof template == "string") {
@@ -38,11 +52,29 @@ class ModTemplate implements LmTemplate {
     this._systemBlock = this._buildSystemBlock();
   }
 
+  /**
+   * Replaces the system block with a given message.
+   * 
+   * @param msg - The message to replace the system block with.
+   * @returns A reference to the current `ModTemplate` instance for chaining.
+   * 
+   * @example
+   * tpl.replaceSystem('You are a javascript expert');
+   */
   replaceSystem(msg: string): ModTemplate {
     this._systemBlock = this._buildSystemBlock(msg);
     return this
   }
 
+  /**
+   * Appends a given message after the system message.
+   * 
+   * @param msg - The message to append.
+   * @returns A reference to the current `ModTemplate` instance for chaining.
+   * 
+   * @example
+   * tpl.afterSystem('You are a javascript expert');
+   */
   afterSystem(msg: string): ModTemplate {
     if (!this.system) {
       throw new Error("This template has no system var")
@@ -54,16 +86,44 @@ class ModTemplate implements LmTemplate {
     return this
   }
 
+  /**
+   * Appends a given message after the assistant prompt token.
+   * 
+   * @param msg - The message to append.
+   * @returns A reference to the current `ModTemplate` instance for chaining.
+   * 
+   * @example
+   * tpl.afterAssistant('. Have a great day!');
+   */
   afterAssistant(msg: string): ModTemplate {
     this.assistant = this.assistant + msg;
     return this
   }
 
+  /**
+   * Replaces the `{prompt}` placeholder in the user message with a given message.
+   * 
+   * @param msg - The message to replace the placeholder with.
+   * @returns A reference to the current `ModTemplate` instance for chaining.
+   * 
+   * @example
+   * tpl.replacePrompt(fix this invalid json:\n\n```json\n{prompt}\n```);
+   */
   replacePrompt(msg: string): ModTemplate {
     this.user = this.user.replace("{prompt}", msg);
     return this
   }
 
+  /**
+   * Adds a new shot (a user-assistant interaction) to the template.
+   * 
+   * @param user - The user's message.
+   * @param assistant - The assistant's response.
+   * @returns A reference to the current `ModTemplate` instance for chaining.
+   * 
+   * @example
+   * tpl.addShot('Is it raining?', 'No, it is sunny.');
+   */
   addShot(user: string, assistant: string): ModTemplate {
     if (!this?.shots) {
       this.shots = [];
@@ -75,6 +135,15 @@ class ModTemplate implements LmTemplate {
     return this
   }
 
+  /**
+   * Renders the template into a string representation.
+   * 
+   * @returns The rendered template as a string.
+   * 
+   * @example
+   * const rendered = tpl.render();
+   * console.log(rendered);
+   */
   render(): string {
     const buf = new Array<string>();
     // system prompt if any
@@ -98,6 +167,16 @@ class ModTemplate implements LmTemplate {
     return buf.join("");
   }
 
+  /**
+  * Renders the template with the provided message replacing the `{prompt}` placeholder.
+  * 
+  * @param msg - The message to use for replacing the `{prompt}` placeholder.
+  * @returns The rendered template with the provided message.
+  * 
+  * @example
+  * const prompted = tpl.prompt("list the planets in the solar system");
+  * console.log(prompted);
+  */
   prompt(msg: string): string {
     return this.render().replace("{prompt}", msg)
   }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,26 +1,136 @@
+/**
+ * Defines the spacing (in terms of line breaks) to be applied between different parts of the conversation.
+ * 
+ * @example
+ * const spacingExample: SpacingSlots = {
+ *   system: 2,
+ *   user: 1,
+ *   assistant: 1
+ * };
+ */
 interface SpacingSlots {
+  /**
+   * Number of line breaks to be applied after the system message.
+   */
   system?: number;
+
+  /**
+   * Number of line breaks to be applied after the user message.
+   */
   user?: number;
+
+  /**
+   * Number of line breaks to be applied after the assistant message.
+   */
   assistant?: number;
 }
 
+/**
+ * Represents a block of system-level prompts or instructions in the conversation.
+ * 
+ * @example
+ * const promptExample: PromptBlock = {
+ *   schema: '### System: {system}',
+ *   message: 'Some system message'
+ * };
+ */
 interface PromptBlock {
+  /**
+   * The schema or format for the system message.
+   * 
+   * Can include placeholders like `{system}` which can be programmatically replaced with actual messages later.
+   */
   schema: string;
+
+  /**
+   * Optional default message content for the system. 
+   * 
+   * Used if a dynamic value isn't provided for `{system}` placeholder.
+   */
   message?: string;
 }
 
+/**
+ * Represents a single turn in a conversation, consisting of a user message followed by an assistant response.
+ * 
+ * @example
+ * const turnExample: TurnBlock = {
+ *   user: 'What's the weather like?',
+ *   assistant: 'It's sunny today!'
+ * };
+ */
 interface TurnBlock {
+  /**
+   * The message content from the user.
+   */
   user: string;
+
+  /**
+   * The corresponding response from the assistant.
+   */
   assistant: string;
 }
 
+/**
+ * Represents a template for language modeling, detailing the structure and interaction elements of a conversation.
+ * 
+ * @example
+ * const sampleTemplate: LmTemplate = {
+ *  name: "Alpaca",
+ *  system: {
+ *    schema: "{system}",
+ *    message: "Below is an instruction that describes a task. Write a response that appropriately completes the request.",
+ *  },
+ *  user: "### Instruction:\n{prompt}",
+ *  assistant: "### Response:",
+ *   linebreaks: {
+ *     system: 2,
+ *     user: 2,
+ *   }
+ * };
+ */
 interface LmTemplate {
+  /**
+   * The name of the template.
+   */
   name: string;
+
+  /**
+   * The default message template for the user.
+   * 
+   * Includes a `{prompt}` placeholder which can be programmatically replaced later.
+   */
   user: string;
+
+  /**
+   * The default message template for the assistant.
+   */
   assistant: string;
+
+  /**
+   * Optional prompt block that represents system-level messages or instructions.
+   */
   system?: PromptBlock;
+
+  /**
+   * Optional array of turn blocks representing back-and-forths between the user and the assistant.
+   * 
+   * Useful for simulating multi-turn interactions.
+   */
   shots?: Array<TurnBlock>;
+
+  /**
+   * Optional array of strings that signal the end of a conversation.
+   * 
+   * These strings can be used to detect when a conversation should be terminated.
+   */
   stop?: Array<string>;
+
+  /**
+   * Optional specifications for line breaks between different message types.
+   * 
+   * This can be used to format the rendered conversation.
+   */
   linebreaks?: SpacingSlots;
 }
 
