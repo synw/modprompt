3c9dc7be34cf9c8e2e666074b03b49ab98752e7f
synw <synwe@yahoo.com>
2025-12-09 14:57:53 +0100

    Refactor one_shot.js example to use new PromptTemplate API and add additional prompt testing
    - Updated import statement to only import PromptTemplate instead of templates and PromptTemplate
    - Changed template initialization from templates.granite to "chatml" format
    - Refactored addShot method to use object-based shot definition instead of separate user/assistant parameters
    - Added new console.log statements to demonstrate the prompt method with different input

--- examples/one_shot.js
+++ examples/one_shot.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
-//import { templates, PromptTemplate } from "modprompt";
-import { templates, PromptTemplate } from "../dist/main.js";
+import { PromptTemplate } from "../dist/main.js";
 
 //console.log("Available templates:", Object.keys(templates));
 const _prompt = "fix this invalid json:\n\n```json\n{prompt}\n```";
 // load template
-const tpl = new PromptTemplate(templates.granite)
+const tpl = new PromptTemplate("chatml")
   .afterSystem(" You are a javascript specialist")
   .afterAssistant("```json")
   .replacePrompt(_prompt)
-  .addShot(
-    _prompt.replace("{prompt}", "{'a':1,}"),
-    '\n{"a":1}\n```',
-  );
+  .addShot({
+    user: _prompt.replace("{prompt}", "{'a':1,} // comment"),
+    assistant: '\n{"a":1}\n```',
+  });
 
 // render the template
-console.log(tpl.render())
+console.log(tpl.render());
+console.log("--------------------------");
+console.log(tpl.prompt("[a, 1', null]"));
